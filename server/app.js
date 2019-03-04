ca46c960cc45f20361f8c50118c992def8bdad6a
dylanalvarez1 <dylanalvarez1@ufl.edu>
2019-03-04 14:41:04 -0500

    initial commit with basic chat

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+server = app.listen(3001, function(){
+    console.log('server is running on port 3001')
+});
+
+const io = require('socket.io')(server);
+io.set('origins', '*:*');
+io.on('connection', function(socket) {
+    console.log(socket.id)
+    socket.on('SEND_MESSAGE', function(data) {
+        console.log("server send message");
+        io.emit('MESSAGE', data);
+        console.log("DATA:" + JSON.stringify(data));
+        
+    });
+});
